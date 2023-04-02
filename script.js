98617f8f97dae5a4d53021796f0fb2720a315d63
Dusan95z <dusan204z@gmail.com>
2023-04-02 18:34:49 +0200

    app finished

---
+++ script.js
@@ -0,0 +1,59 @@
+const checkBtn = document.querySelector('.check');
+const againBtn = document.querySelector('.again');
+const message = document.querySelector('.message');
+const number = document.querySelector('.number');
+const guess = document.querySelector('.guess');
+const scoreEl = document.querySelector('.score');
+const highscoreEl = document.querySelector('.highscore');
+const body = document.querySelector('body');
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+// check button functionality
+checkBtn.addEventListener('click', (e) => {
+  const guess = +document.querySelector('.guess').value;
+  // no input
+  if (!guess) {
+    displayMessage('No number!');
+    // correct guess
+  } else if (guess === secretNumber) {
+    displayMessage('Correct number!');
+    number.textContent = secretNumber;
+    body.style.backgroundColor = '#60b347';
+    number.style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      highscoreEl.textContent = highscore;
+    }
+    // wrong guess
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? 'Too high!' : 'Too low!');
+      score--;
+      scoreEl.textContent = score;
+    } else {
+      displayMessage('You lost the game!');
+      scoreEl.textContent = 0;
+    }
+  }
+});
+
+// again button functionality
+againBtn.addEventListener('click', (e) => {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  displayMessage('Start guessing...');
+  scoreEl.textContent = score;
+  number.textContent = '?';
+  guess.value = '';
+  body.style.backgroundColor = '#222';
+  number.style.width = '15rem';
+});
+
+// message function
+function displayMessage(msg) {
+  message.textContent = msg;
+}
